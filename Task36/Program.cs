f188f66f436eddca9c1662cc58455cee665bfae6
Maksim <brazhmax@mail.ru>
2022-08-22 12:34:36 +0300

    add Task 36

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+
+using static System.Console;
+Clear();
+
+int[] arr = new int[4];
+fillArr(arr);
+WriteLine($"[{String.Join(",", arr)}]-> {oddSum(arr)}");
+
+void fillArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-50, 51);
+    }
+}
+int oddSum(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0) sum += arr[i];
+    }
+    return sum;
+}
